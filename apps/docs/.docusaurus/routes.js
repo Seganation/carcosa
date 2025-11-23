d58d978fb14d211ecc18dad2b47d427faadeaef9
Seganation <xdniklaus@yahoo.com>
2025-11-24 01:25:26 +0300

    fix(docs): Update last updated metadata and add debug routes

--- apps/docs/.docusaurus/routes.js
+++ apps/docs/.docusaurus/routes.js
@@ -3,79 +3,114 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
+    path: '/__docusaurus/debug',
+    component: ComponentCreator('/__docusaurus/debug', '5ff'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/config',
+    component: ComponentCreator('/__docusaurus/debug/config', '5ba'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/content',
+    component: ComponentCreator('/__docusaurus/debug/content', 'a2b'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/globalData',
+    component: ComponentCreator('/__docusaurus/debug/globalData', 'c3c'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/metadata',
+    component: ComponentCreator('/__docusaurus/debug/metadata', '156'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/registry',
+    component: ComponentCreator('/__docusaurus/debug/registry', '88c'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/routes',
+    component: ComponentCreator('/__docusaurus/debug/routes', '000'),
+    exact: true
+  },
+  {
     path: '/search',
     component: ComponentCreator('/search', '5de'),
     exact: true
   },
   {
     path: '/',
-    component: ComponentCreator('/', '344'),
+    component: ComponentCreator('/', 'f78'),
     routes: [
       {
         path: '/',
-        component: ComponentCreator('/', '726'),
+        component: ComponentCreator('/', 'fa7'),
         routes: [
           {
             path: '/',
-            component: ComponentCreator('/', 'bf4'),
+            component: ComponentCreator('/', '3ac'),
             routes: [
               {
                 path: '/authentication',
-                component: ComponentCreator('/authentication', 'f01'),
+                component: ComponentCreator('/authentication', '153'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
                 path: '/configuration',
-                component: ComponentCreator('/configuration', '21e'),
+                component: ComponentCreator('/configuration', '7ff'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
                 path: '/examples/basic-upload',
-                component: ComponentCreator('/examples/basic-upload', '01a'),
+                component: ComponentCreator('/examples/basic-upload', 'a25'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
                 path: '/file-router/overview',
-                component: ComponentCreator('/file-router/overview', 'a6f'),
+                component: ComponentCreator('/file-router/overview', '7eb'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
                 path: '/installation',
-                component: ComponentCreator('/installation', '63c'),
+                component: ComponentCreator('/installation', '6ad'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
                 path: '/migration/from-uploadthing',
-                component: ComponentCreator('/migration/from-uploadthing', '4ab'),
+                component: ComponentCreator('/migration/from-uploadthing', '633'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
                 path: '/nextjs/components',
-                component: ComponentCreator('/nextjs/components', '3fa'),
+                component: ComponentCreator('/nextjs/components', 'b6d'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
                 path: '/nextjs/setup',
-                component: ComponentCreator('/nextjs/setup', '3f0'),
+                component: ComponentCreator('/nextjs/setup', '3f6'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
                 path: '/quick-start',
-                component: ComponentCreator('/quick-start', '198'),
+                component: ComponentCreator('/quick-start', 'b77'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
                 path: '/',
-                component: ComponentCreator('/', '94e'),
+                component: ComponentCreator('/', '2d7'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               }
